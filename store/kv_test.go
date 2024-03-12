b308b917780856fbc3bc548fca2adf0b0b44de3e
Steven DeLeon <Steven.DeLeon@bjss.com>
2024-03-12 13:33:30 -0400

    command delete and actor set tests

--- store/kv_test.go
+++ store/kv_test.go
@@ -1,6 +1,15 @@
 package store
 
-import "testing"
+import (
+	"bytes"
+	"log"
+	"os"
+	"strings"
+	"testing"
+	"time"
+)
+
+// Command tests
 
 func TestCommandSet_Apply(t *testing.T) {
 	actor := NewActor()
@@ -67,6 +76,79 @@ func TestCommandGet_Apply(t *testing.T) {
 		if err == nil {
 			t.Errorf("Expected error but got nil")
 		}
+	}
+}
+
+func TestCommandDelete_Apply(t *testing.T) {
+	actor := NewActor()
+
+	cmdSet := CommandSet{Key: "hello", Value: "world", Error: make(chan error)}
+	err := cmdSet.Apply(actor)
+	if err != nil {
+		t.Errorf("Error applying command: %v", err)
+	}
+
+	// happy path
+	cmdDel := CommandDelete{Key: "hello", Error: make(chan error)}
+	err = cmdDel.Apply(actor)
+	if err != nil {
+		t.Errorf("Error applying command: %v", err)
+	}
+
+	// error case
+	cmdDel = CommandDelete{Key: "hello_notfound", Error: make(chan error)}
+	err = cmdDel.Apply(actor)
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+
+	// check if key was deleted
+	cmdGet := CommandGet{Key: "hello", Response: make(chan string, 1), Error: make(chan error)}
+	go func() {
+		err = cmdGet.Apply(actor)
+		if err != nil {
+			t.Errorf("Error applying command: %v", err)
+		}
+	}()
+
+	select {
+	case res := <-cmdGet.Response:
+		t.Errorf("Expected empty response but got %s", res)
+	case err = <-cmdGet.Error:
+		if err == nil {
+			t.Errorf("Expected error but got nil")
+		}
+	}
+}
+
+// Actor tests
+
+func TestActor_Set(t *testing.T) {
+	// buffer
+	buf := &bytes.Buffer{}
+	log.SetOutput(buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+
+	actor := NewActor()
+
+	// happy path
+	go actor.Set("hello", "world")
+	time.Sleep(time.Second)
+
+	val, err := actor.Get("hello")
+	if err != nil || val != "world" {
+		t.Errorf("Unexpected error or value after Set: %v, %s", err, val)
+	}
+
+	// error case
+	go actor.Set("hello", "world2")
+	time.Sleep(time.Second)
+
+	logs := buf.String()
+	if !strings.Contains(logs, "Error applying command: key hello already exists") {
+		t.Errorf("Expected error message but got %s", logs)
 	}
 }
 
